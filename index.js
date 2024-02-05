eca65231e5db1df0c98e4d8658a47038438d4f72
Mr. Sanjoy <--add>
2024-02-05 11:41:17 +0530

    f commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const PORT = 7788;
+
+app.get('/', function (req, res) {
+    res.send('Express JS Server')
+})
+app.listen(PORT, () => {
+    console.log("Express server listening on port:", PORT);
+});
