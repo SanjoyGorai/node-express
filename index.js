f4174c6d564dd69b053571001954a8b0ec5bb503
Mr. Sanjoy <--add>
2024-02-05 19:24:39 +0530

    postgres connected

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from 'express';
-const app = express();
+import cors from 'cors';
 const PORT = 3000;
+import pg from 'pg';
+const { Pool } = pg;
+const app = express();
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'student',
+    user: 'postgres',
+    password: 'sanjoypql'
+});
+
+pool.connect((error)=>{
+    if (error) {
+        console.log(error);
+    }
+    console.log("Postgres connected successfully");
+})
+
+// const result = pool.query('SELECT * FROM postgres')
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
+
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.send('Express JS Server');
@@ -17,5 +38,5 @@ app.get('/images', (req, res) => {
 
 
 app.listen(PORT, () => {
-    console.log("Express server listening on port:", PORT);
+    console.log(`Express server listening on port http://localhost:${PORT}`);
 });
