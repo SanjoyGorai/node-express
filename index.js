ad862856a6334be9004f27ef2ebe072e5c66a193
Mr. Sanjoy <--add>
2024-02-05 12:38:53 +0530

    basic ejs template is created

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import ejs from 'ejs';
-
 const app = express();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
-
 app.get('/', function (req, res) {
     res.send('Express JS Server');
 });
 app.get('/ejs', (req, res) => {
     res.render('index');
+});
+
+app.get('/images', (req, res) => {
 });
 
 
