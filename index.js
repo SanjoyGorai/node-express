3be2192db616228bb47967ccac67a81ce55d7dbe
Mr. Sanjoy <--add>
2024-02-05 11:44:08 +0530

    ejs i

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 const PORT = 7788;
 
 app.get('/', function (req, res) {
-    res.send('Express JS Server')
-})
+    res.send('Express JS Server');
+});
+
+
 app.listen(PORT, () => {
     console.log("Express server listening on port:", PORT);
 });
